393dd65edc8e787d2ab1e7b2a9bae6148768e40f
Alon Kerklies <alonkerklies@gmail.com>
2024-07-17 09:47:03 +0000

    finish add many

--- schema/Resource_schema.js
+++ schema/Resource_schema.js
@@ -18,6 +18,29 @@ const post_resource_schema = {
 
 
   }
-  module.exports = {post_resource_schema}
+
+
+  const post_many_resources_schema = {
+
+    type: "object",
+    properties: {
+      // resource_id: {type: "string"},
+      resource_string: {type: "string" , "minLength": 3},
+      monitoring: {type: "boolean"},
+      description: {type: "string"},
+      item_tool_list: {type: "array"},
+      // item_types_list: {type: "array" ,"minItems": 1},
+    //   foo: {type: "integer"},
+    },
+
+
+
+    required: ["resource_string"],
+    additionalProperties: true
+
+
+  }
+
+  module.exports = {post_resource_schema,post_many_resources_schema}
 
  
