77b2e41c104eca5cfd61a9a6fea961cb09f2bd0e
D-V-W <david.v.wolfson@gmail.com>
2024-07-30 14:42:26 +0300

    AlertsThursday

--- controllers/AlettController.js
+++ controllers/AlettController.js
@@ -1,15 +1,34 @@
-const { GetAlertsFile } = require("../models/AlertModal");
+const { GetAlertsFile, UpdateAlertFile } = require("../models/AlertModal");
 
 async function GetAlertFileData(req, res, next) {
   try {
-    const [data] = await GetAlertsFile();
-    console.log(data, "sdsdsdsdsdsdsdsdsd");
+    const data = await GetAlertsFile();
+    // console.log(data, "sdsdsdsdsdsdsdsdsd");
     res.send(data);
   } catch (error) {
     console.log(error, "GetAlertFileData GetAlertFileData");
   }
 }
 
+async function UpdateAlertFileData(req, res, next) {
+  try {
+    console.log("hello update ", req.body);
+    const { Info } = req.body;
+    Info.UserInput.UserId = "User";
+
+    const up = await UpdateAlertFile(Info);
+    if (up) {
+      res.send(up);
+    } else {
+      res.status(404).send("No Such alert");
+    }
+  } catch (err) {
+    console.log("Error In Update alert file ", err);
+    res.status(404).send({ msg: "Error in update", error: err });
+  }
+}
+
 module.exports = {
   GetAlertFileData,
+  UpdateAlertFileData,
 };
