b01db22490d773ca3868bdd8e5cd891512f2f374
D-V-W <david.v.wolfson@gmail.com>
2024-08-21 14:14:27 +0300

    delete history

--- controllers/LogsController.js
+++ controllers/LogsController.js
@@ -1,29 +1,24 @@
+const { get_log_model } = require("../models/LogsModels.js");
+const DBConnection = require("../db.js");
 
- const { get_log_model } = require('../models/LogsModels.js');
- const DBConnection = require('../db.js')
- 
 async function Get_Log(req, res, next) {
+  const { logName } = req.query;
+  const { fileName } = req.query;
 
- const {logName} = req.query
- const {fileName} = req.query
+  if (logName === undefined) {
+    console.log("logName is ", logName);
+    return;
+  }
 
- if (logName === undefined) {console.log("logName is " , logName); return}
+  try {
+    const log_file_data = await get_log_model(logName, fileName);
 
-
-  try{
-    const log_file_data = await get_log_model(logName,fileName)  
-
-
- res.send(log_file_data)
-  }catch(err)
-  {console.log("errrrrrrrrrrrrrrr",err);}
-
+    res.send(log_file_data);
+  } catch (err) {
+    console.log("errrrrrrrrrrrrrrr", err);
+  }
 }
- 
- 
 
 module.exports = {
-
   Get_Log,
- 
 };
